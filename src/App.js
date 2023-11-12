5afa68ce0f6b806f8b39c4011a62b18801708580
ibone22 <ibonehernandez22@gmail.com>
2023-11-12 15:15:42 -0500

    Carga inicial del proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import FirstComponent from "./components/FirstComponent"
+import "./styles.css"
 
 function App() {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    console.log("Ejecutandose")
+    //consumo de la api de leer productos
+    fetch('https://pro-04cx.onrender.com/products', {
+      method: "GET"
+    }).then(response => response.json()
+    .then(data => setProducts(data))
+    .catch(err => console.log(err))
+    )
+    .catch(errorResponse => console.log(errorResponse))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FirstComponent></FirstComponent>
+      <FirstComponent/>
+      {products.length === 0 ?
+      <h3>No hay productos</h3>:
+      products.map(producto => <div key = {producto.name} className="fondo">
+      <h3>Nombre: {producto.name}</h3>
+      <p>Cantidad: {producto.cantidad}</p>
+      <p>Precio: {producto.precio}</p>
+      </div>)
+      }
+    </>
   );
 }
 
