a8e493fdf8baa04a1b358a3b52078a76163967e6
ibone22 <ibonehernandez22@gmail.com>
2023-11-12 16:21:11 -0500

    Se agrega react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import FirstComponent from "./components/FirstComponent"
 import "./styles.css"
 
@@ -10,25 +11,31 @@ function App() {
     fetch('https://pro-04cx.onrender.com/products', {
       method: "GET"
     }).then(response => response.json()
-    .then(data => setProducts(data))
-    .catch(err => console.log(err))
+      .then(data => setProducts(data))
+      .catch(err => console.log(err))
     )
-    .catch(errorResponse => console.log(errorResponse))
+      .catch(errorResponse => console.log(errorResponse))
   }, [])
 
   return (
-    <>
-      <FirstComponent></FirstComponent>
-      <FirstComponent/>
-      {products.length === 0 ?
-      <h3>No hay productos</h3>:
-      products.map(producto => <div key = {producto.name} className="fondo">
-      <h3>Nombre: {producto.name}</h3>
-      <p>Cantidad: {producto.cantidad}</p>
-      <p>Precio: {producto.precio}</p>
-      </div>)
-      }
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<>
+          <FirstComponent></FirstComponent>
+          <FirstComponent />
+          {products.length === 0 ?
+            <h3>No hay productos</h3> :
+            products.map(producto => <div key={producto.name} className="fondo">
+              <h3>Nombre: {producto.name}</h3>
+              <p>Cantidad: {producto.cantidad}</p>
+              <p>Precio: {producto.precio}</p>
+            </div>)
+          }
+        </>} />
+        <Route path="*" element={<h1>Pagina no encontrada</h1>}/>
+      </Routes>
+
+    </HashRouter>
   );
 }
 
