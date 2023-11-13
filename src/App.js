97b140ba86f7e51e875866f670d9a11aa81222f9
ibone22 <ibonehernandez22@gmail.com>
2023-11-12 23:03:24 -0500

    Carga final

--- src/App.js
+++ src/App.js
@@ -17,22 +17,47 @@ function App() {
       .catch(errorResponse => console.log(errorResponse))
   }, [])
 
+  const [Admin, setAdmin] = useState([])
+  useEffect(() => {
+    console.log("Ejecutandose")
+    //consumo de la api de leer productos
+    fetch('https://pro-04cx.onrender.com/users/admin', {
+      method: "GET"
+    }).then(response => response.json()
+      .then(data => setAdmin(data))
+      .catch(err => console.log(err))
+    )
+      .catch(errorResponse => console.log(errorResponse))
+  }, [])
+
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<>
-          <FirstComponent></FirstComponent>
-          <FirstComponent />
-          {products.length === 0 ?
+          {products.length === 0 ? (
+            <h3>No hay productos</h3>
+          ) : (
+            products.map(producto => (
+              <div key={producto.name} className="fondo">
+                <h3>Nombre: {producto.name}</h3>
+                <p>Cantidad: {producto.cantidad}</p>
+                <p>Precio: {producto.precio}</p>
+              </div>
+            ))
+          )}
+
+        </>} />
+        <Route path="/" element={<>
+          {Admin.length === 0 ?
             <h3>No hay productos</h3> :
-            products.map(producto => <div key={producto.name} className="fondo">
-              <h3>Nombre: {producto.name}</h3>
-              <p>Cantidad: {producto.cantidad}</p>
-              <p>Precio: {producto.precio}</p>
+            Admin.map(administrador => <div key={administrador.user} className="fondo">
+              <h3>Nombre: {administrador.user}</h3>
+              <p>Email: {administrador.email}</p>
             </div>)
           }
         </>} />
-        <Route path="*" element={<h1>Pagina no encontrada</h1>}/>
+
+        <Route path="*" element={<h1>Pagina no encontrada</h1>} />
       </Routes>
 
     </HashRouter>
@@ -40,3 +65,4 @@ function App() {
 }
 
 export default App;
+
